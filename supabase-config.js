58025d0750ce42e9b7c827d338af0f5022b208e6
victornascimentodiniz <130411984+victornascimentodiniz@users.noreply.github.com>
2026-08-12 21:15:29 -0300

    Update Supabase publishable key in configuration

--- supabase-config.js
+++ supabase-config.js
@@ -1,11 +1,26 @@
+/* =========================================================
+   CONFIGURAÇÃO DO SUPABASE
+   ACAMPAMENTO RENOVO
+========================================================= */
+
 const SUPABASE_URL =
     "https://xgfywvvgtckjhypqjegv.supabase.co";
 
 const SUPABASE_PUBLISHABLE_KEY =
-    "sb_publishable_abc123xyz456";
+    "sb_publishable_rqveXjLAEhEaA309HlC2hQ_a7Pd2INL";
 
-const supabaseClient =
+
+/* =========================================================
+   CRIAR CONEXÃO COM O SUPABASE
+========================================================= */
+
+window.supabaseClient =
     window.supabase.createClient(
         SUPABASE_URL,
         SUPABASE_PUBLISHABLE_KEY
     );
+
+
+console.log(
+    "Supabase conectado com sucesso!"
+);
